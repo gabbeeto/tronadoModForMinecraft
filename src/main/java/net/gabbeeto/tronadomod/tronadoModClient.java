3ac95f81d481a71ec1670b655f13f314174682f8
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-11-13 10:21:13 -0300

    Initial commit

---
+++ src/main/java/net/gabbeeto/tronadomod/tronadoModClient.java
@@ -0,0 +1,12 @@
+package net.gabbeeto.tronadomod;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class tronadoModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/gabbeeto/tronadomod/tronadoModTestDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gabbeeto.tronadomod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class tronadoModTestDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
